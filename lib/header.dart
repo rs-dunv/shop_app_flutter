fd5d27e67be68fbcea36b6f588259ef11acfb855
rs-dunv <dunv@runsystem.net>
2021-03-01 16:01:26 +0700

    update

---
+++ lib/header.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainHeaderApp extends StatefulWidget {
+  @override
+  _HeaderApp createState() => _HeaderApp();
+}
+
+class _HeaderApp extends State<MainHeaderApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Header'),
+    );
+  }
+}
