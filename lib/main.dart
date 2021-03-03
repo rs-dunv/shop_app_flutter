bda597a63542c93b182270388124c8c66afbdd5e
rs-dunv <dunv@runsystem.net>
2021-03-03 11:33:07 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flant App',
+      title: 'Flant App!',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primaryColor: kPrimaryColor,
