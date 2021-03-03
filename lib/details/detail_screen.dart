95a4c23c1e534cdaed94ae29947f1a90ce99d176
rs-dunv <dunv@runsystem.net>
2021-03-03 11:20:25 +0700

    update

--- lib/details/detail_screen.dart
+++ lib/details/detail_screen.dart
@@ -1,13 +1,15 @@
+import 'package:ShopApp/details/components/body_detail.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-    );
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+        ),
+        child: BodyDetail());
   }
 }
